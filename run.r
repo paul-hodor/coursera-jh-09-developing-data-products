f7b0f7bfb508c8f10c8fdc76bebe8607f232b247
Paul Hodor <hodor_paul@bah.com>
2015-08-22 19:29:34 -0400

    publishing deck

--- run.r
+++ run.r
@@ -10,22 +10,4 @@ browseURL("index.html")
 
 ## use the arrow keys to navigate slides
 
-
-v <- matrix(c(2,1),ncol=1)
-
-par(mar=c(4,4,1,1))
-plot(0,0,
-     xlim=c(-4,4), ylim=c(-4,4),pch=NA,xlab="x",ylab="y")
-arrows(0,0,v[1],v[2],length=0.1,col="black")
-
-A.sc = matrix(c(-2,0,0,-2),ncol=2)
-vp.sc = A.sc %*% v
-arrows(0,0,vp.sc[1],vp.sc[2],length=0.1,col="blue")
-
-A.st = matrix(c(2,0,0,1),ncol=2)
-vp.st = A.st %*% v
-arrows(0,0,vp.st[1],vp.st[2],length=0.1,col="red")
-
-A.ro = matrix(c(sqrt(3)/2,1/2,-1/2,sqrt(3)/2),ncol=2)
-vp.ro = A.ro %*% v
-arrows(0,0,vp.ro[1],vp.ro[2],length=0.1,col="green")
+publish(user="MoohBear", repo="Developing-Data-Products-2015-08")
